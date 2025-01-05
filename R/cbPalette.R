b6a42458cd972228731f67b5e53cd825f0b91eb1
hannahas <ahannah95@gmail.com>
2025-01-05 10:55:16 -0800

    More editing, wrote document()

--- R/cbPalette.R
+++ R/cbPalette.R
@@ -5,7 +5,23 @@ cbColors <- list(
   cb7 = c('red', 'blue', 'grey39', 'lightsteelblue2', 'orange', 'black', 'pink3')
 )
 
-# function that generates an actual colour palette from our simple list of colours
+#' Generate an actual colour palette from our simple list of colours
+#'
+#' @param name Name of the color palette we want to use
+#' @param n How many colors to use (default to full length of palette)
+#' @param all_palettes the list of colour palettes we want to extract our choice from
+#' @param type discrete or continuous palette
+#'
+#' @return a color palette
+#' @export
+#'
+#' @examples
+#' cbColors <- list(
+#'   cvi_purples = c("#381532", "#4b1b42", "#5d2252", "#702963",
+#'                 "#833074", "#953784", "#a83e95"),
+#'   cb7 = c('red', 'blue', 'grey39', 'lightsteelblue2', 'orange', 'black', 'pink3')
+#' )
+#' mypalette <- cvi_palettes("cb7", type = "discrete")
 cvi_palettes <- function(name, n, all_palettes = cbColors, type = c("discrete", "continuous")) {
   palette = all_palettes[[name]]
   if (missing(n)) {
@@ -20,10 +36,59 @@ cvi_palettes <- function(name, n, all_palettes = cbColors, type = c("discrete", 
 }
 
 ######################## functions so that {ggplot2} understands what to do with our colour palettes
+#' Function for ggplot to recognize palette (colour, for geom_point)
+#'
+#' @param name color palette to use
+#'
+#' @return ggplot palette
+#' @importFrom ggplot2 ggplot aes
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' months <- 24
+#' person_names <- c("Adam", "Matt", "Sarah", "Ben", "Katie", "Cara", "Bob")
+#' df <- expand.grid(month = 1:months, person = person_names)
+#' initial_weights <- runif(length(person_names), min = 120, max = 180)
+#' weights_map <- setNames(initial_weights, person_names)
+#' df$weight <- weights_map[as.character(df$person)]
+#' df$weight <- df$weight * (1 + runif(nrow(df), -0.1, 0.1))
+#' df$person <- factor(df$person)
+#' plt <- ggplot(df, mapping = aes(x = month, y = weight)) +
+#' scale_y_continuous(limits = c(100, 230))
+#' plt +
+#' geom_point(aes(colour = person), size = 3) +
+#'   geom_line(aes(color = person), linewidth = 1) +
+#'   scale_colour_cvi_d("cb7")
 scale_colour_cvi_d = function(name) {
   ggplot2::scale_colour_manual(values = cvi_palettes(name,
                                                      type = "discrete"))
 }
+
+#' Function for ggplot to recognize palette (fill, for geom_bar)
+#'
+#' @param name color palette to use
+#'
+#' @return ggplot palette
+#' @importFrom ggplot2 ggplot aes
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' months <- 24
+#' person_names <- c("Adam", "Matt", "Sarah", "Ben", "Katie", "Cara", "Bob")
+#' df <- expand.grid(month = 1:months, person = person_names)
+#' initial_weights <- runif(length(person_names), min = 120, max = 180)
+#' weights_map <- setNames(initial_weights, person_names)
+#' df$weight <- weights_map[as.character(df$person)]
+#' df$weight <- df$weight * (1 + runif(nrow(df), -0.1, 0.1))
+#' df$person <- factor(df$person)
+#' plt <- ggplot(df, mapping = aes(x = month, y = weight)) +
+#' scale_y_continuous(limits = c(100, 230))
+#' plt +
+#' geom_point(aes(colour = person), size = 3) +
+#'   geom_line(aes(color = person), linewidth = 1) +
+#'   scale_fill_cvi_d("cb7")
 scale_fill_cvi_d = function(name) {
   ggplot2::scale_fill_manual(values = cvi_palettes(name,
                                                    type = "discrete"))
--- toMakePackage.R
+++ toMakePackage.R
@@ -13,6 +13,45 @@ load_all()
 strsplit1(x, split = ",")
 # Run the code in 'testcbPalette.R' to see if cbPalette works
 # runCodeHere
-# Now make initial commit to Github by running on Terminal:
 
+# Now make initial commit to Github by running on Terminal:
+# cd ~/Desktop/cbPalette
+# git init
+# git add .
+# git commit -m "Initial commit"
+# git remote add origin https://github.com/hannahas/cbPalette.git
+# git push -u origin master
+
+# Now check - this doesn't work for me, what am I doing wrong?
+# diff --git a/R/cbPalette.R b/R/cbPalette.R
+
+# Check
+check()
+
+# Got warning about not declaring ggplot2, so lets do that
+use_package("ggplot2")
+# That warning disappeared now, great!
+
+# To configure a valid license for the package, call use_mit_license()
+use_mit_license()
+
+# open R/strsplit1.R in the source editor and put the cursor somewhere in the strsplit1() function definition.
+# Now do Code > Insert roxygen skeleton.
+# modify it
+# trigger the conversion of this new roxygen comment into man/strsplit1.Rd with document()
+document()
+
+# preview help files for the functions I wrote them for
+?strsplit1
+?cvi_palettes
+?scale_colour_cvi_d
+?scale_fill_cvi_d
+
+check()
+# Getting error about function
+
+rm(list = c("cvi_palettes"))
+check()
+
+install()
 
